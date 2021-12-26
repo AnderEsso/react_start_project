8f2c2181d17bafb8c3602a92034a2758533f39e9
AnderEsso <andr03409@gmail.com>
2021-12-27 01:20:15 +0200

    added logic for Landing Comonents

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,12 +4,37 @@ import React from "react"
 import restaraunts from "../sample-restaurants"
 
 class Landing extends React.Component {
+  state = {
+    display: false,
+    title: "",
+    url: "",
+  }
+
+  displayList = () => {
+    const { display } = this.state
+    this.setState({
+      display: !display,
+    })
+  }
+
+  getTitle = (restaraunt) => {
+    const { title, url } = restaraunt
+    this.setState({
+      title,
+      url,
+      display: false,
+    })
+  }
+
   render() {
     return (
       <div className="restaraunt_select">
         <div className="restaraunt_select_top">
-          <div className="restaraunt_select_top-header font-effect-outline">
-            Выбери ресторан
+          <div
+            onClick={this.displayList}
+            className="restaraunt_select_top-header font-effect-outline"
+          >
+            {this.state.title ? this.state.title : "Выбери ресторан"}
           </div>
 
           <div className="arrow_picker">
@@ -17,14 +42,25 @@ class Landing extends React.Component {
             <div className="arrow_picker-down"></div>
           </div>
         </div>
-        <div className="restaraunt_select_bottom">
-          <ul>
-            {restaraunts.map((restaraunt) => {
-              return <li key={restaraunt.id}>{restaraunt.title}</li>
-            })}
-          </ul>
-        </div>
-        <button>Перейти в ресторан</button>
+        {this.state.display ? (
+          <div className="restaraunt_select_bottom">
+            <ul>
+              {restaraunts.map((restaraunt) => {
+                return (
+                  <li
+                    onClick={() => this.getTitle(restaraunt)}
+                    key={restaraunt.id}
+                  >
+                    {restaraunt.title}
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        ) : null}
+        {this.state.title && !this.state.display ? (
+          <button>Перейти в ресторан</button>
+        ) : null}
       </div>
     )
   }
